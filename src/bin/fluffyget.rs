315dcdeb88020338a91935779f531d891a779bb0
Ezra Alvarion <ezraalvarion71@gmail.com>
2024-01-07 15:11:42 +0700

    feat: finished pools subcommand for fluffyget

--- src/bin/fluffyget.rs
+++ src/bin/fluffyget.rs
@@ -9,73 +9,124 @@
 mod common;
 
 use {
-    std::error::Error as stdError,
-    clap::{command, Parser, arg, Subcommand},
+    std::{
+        error::Error as stdError,
+        path::PathBuf,
+        fs,
+    },
+    clap::{Command, arg},
     common::build_header,
-    futures::StreamExt,
-    fluffyf::{connect::*, api::{post::PostWrapper, pool::Pool}},
-    tokio::{fs::File, io::AsyncWriteExt},
+    futures::{StreamExt, stream, executor::block_on},
+    fluffyf::{connect::*, api::{PostWrapper, Pool, List, IdType}},
 };
 
+fn cli() -> Command {
+    Command::new(env!("CARGO_BIN_NAME"))
+        .about("A fast asynchronous E621/E926 post(s)/pool(s) downloader")
+        .version(env!("CARGO_PKG_VERSION"))
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .subcommand(
+            Command::new("posts")
+                .alias("post")
+                .about("Fetch and downloads posts")
+                .allow_missing_positional(true)
+                .arg_required_else_help(true)
+                .arg(
+                    arg!(-o --output <PATH> "Sets the output path, defaults to current directory")
+                        .default_value(".")
+                        .value_parser(clap::value_parser!(PathBuf))
+                )
+                .arg(
+                    arg!(<ID> ... "The ID of posts to download")
+                        .required(true)
+                        .value_parser(clap::value_parser!(IdType))
+                )
+        )
+        .subcommand(
+            Command::new("pools")
+                .alias("pool")
+                .about("Fetch and downloads pools")
+                .allow_missing_positional(true)
+                .arg_required_else_help(true)
+                .arg(
+                    arg!(-o --output <PATH> "Sets the output path, defaults to current directory")
+                        .default_value(".")
+                        .value_parser(clap::value_parser!(PathBuf))
+                )
+                .arg(arg!(-N --name "If used, make the pool folders name from pool names instead of from ID"))
+                .arg(
+                    arg!(<ID> ... "The ID of pools to download")
+                        .required(true)
+                        .value_parser(clap::value_parser!(IdType))
+                )
+        )
+}
+
 #[tokio::main]
-async fn download_pool() -> Result<(), Box<dyn stdError>> {
-    let client = create_client(build_header("ostipyroxene", "tCEt2CifHzRzMAcakJuEYpbx"))?;
-    let pool = Pool::new_by_id(client.clone(), 37853).await?;
-    println!("{:#?}", pool);
-    let posts = pool.get_all_posts(client.clone()).await?;
-    futures::stream::iter(posts.iter().map(|postw| {
+async fn download_posts(path: &PathBuf, posts: Vec<&IdType>) -> Result<(), Box<dyn stdError>> {
+    let client = create_client(build_header("ostipyroxene", "tCEt2CifHzRzMAcakJuEYpbx")).unwrap();
+    futures::stream::iter(posts.iter().map(|id| {
         let cc = client.clone();
+        let p = path.clone();
         async move {
-            match postw {
-                PostWrapper::Post(post) => {
+            match PostWrapper::new_by_id(cc.clone(), **id).await {
+                Ok(PostWrapper::Post(post)) => {
                     match post.file.get_image_data(cc).await {
                         Ok(b) => {
-                            File::create(format!("volleyball/{}.png", post.id))
-                            .await.unwrap()
-                            .write(b.as_ref())
-                            .await.unwrap();
+                            fs::write(format!("{}/{}.png", p.display(), id), b.as_ref())
+                            .unwrap();
                         },
                         Err(_) => panic!()
                     }
                 }
+                Err(_) => println!("error: post of id {} not found", id)
             }
         }
     })).buffer_unordered(20).collect::<Vec<()>>().await;
     Ok(())
 }
 
-// #[derive(Parser)]
-// #[command(author, version, about, long_about = None)]
-// struct Cli {
-//     /// Optional name to operate on
-//     name: Option<String>,
-
-//     /// Sets a custom config file
-//     #[arg(short, long, value_name = "FILE")]
-//     config: Option<PathBuf>,
-
-//     /// Turn debugging information on
-//     #[arg(short, long, action = clap::ArgAction::Count)]
-//     debug: u8,
-
-//     #[command(subcommand)]
-//     command: Option<Commands>,
-// }
-
-// #[derive(Subcommand)]
-// enum Commands {
-//     /// does testing things
-//     Test {
-//         /// lists test values
-//         #[arg(short, long)]
-//         list: bool,
-//     },
-// }
-
 fn main() -> Result<(), Box<dyn stdError>> {
     // Starts the logger
     // TODO: change the logger implementation to env_logger
     simple_logger::SimpleLogger::new().init().unwrap();
+
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("posts", sm)) => {
+            let path = sm
+                .get_one::<PathBuf>("output")
+                .expect("defaulted in clap");
+
+            let ids = sm
+                .get_many::<IdType>("ID")
+                .into_iter()
+                .flatten()
+                .collect::<Vec<_>>();
+
+            println!("These ids: {:?} to here: {:?}", ids, path);
+            let _ = download_posts(path, ids);
+        }
+        Some(("pools", sm)) => {
+            let path = sm
+                .get_one::<PathBuf>("output")
+                .expect("defaulted in clap");
+            
+            let name = sm.contains_id("name");
+
+            let ids = sm
+                .get_many::<IdType>("ID")
+                .into_iter()
+                .flatten()
+                .collect::<Vec<_>>();
+
+            println!("These ids: {:?} to here: {:?} and name is {}", ids, path, name);
+        }
+        _ => unreachable!("parser should ensure only valid subcommand names are used"),
+    }    
 
     Ok(())
 }
