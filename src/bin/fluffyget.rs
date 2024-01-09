a1bc911b27a4586d38e17b13bad578c256c67447
Ezra Alvarion <ezraalvarion71@gmail.com>
2024-01-09 20:48:41 +0700

    feat: pool downloading

--- src/bin/fluffyget.rs
+++ src/bin/fluffyget.rs
@@ -6,9 +6,6 @@
 // at Your option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use clap::{ArgAction, ArgMatches, Arg};
-use log::info;
-
 mod common;
 
 use {
@@ -17,10 +14,10 @@ use {
         path::PathBuf,
         fs,
     },
-    clap::{Command, arg},
+    clap::{Command, arg, ArgAction, Arg},
     common::build_header,
-    futures::{StreamExt, stream, executor::block_on},
-    fluffyf::{connect::*, api::{PostWrapper, Pool, List, IdType}},
+    futures::StreamExt,
+    fluffyf::{connect::*, api::{PostWrapper, Post, Pool, List, IdType}},
 };
 
 fn cli() -> Command {
@@ -78,8 +75,7 @@ fn cli() -> Command {
         )
 }
 
-#[tokio::main]
-async fn download_posts(client: reqwest::Client, path: &PathBuf, posts: Vec<&IdType>) -> Result<(), Box<dyn stdError>> {
+async fn download_posts_id(client: reqwest::Client, path: &PathBuf, posts: Vec<&IdType>) -> Result<(), Box<dyn stdError>> {
     futures::stream::iter(posts.iter().map(|id| {
         let cc = client.clone();
         let p = path.clone();
@@ -94,10 +90,48 @@ async fn download_posts(client: reqwest::Client, path: &PathBuf, posts: Vec<&IdT
                         Err(_) => panic!()
                     }
                 }
-                Err(_) => println!("error: post of id {} not found", id)
+                Err(_) => log::error!("error: post of id {} not found", id)
             }
         }
     })).buffer_unordered(20).collect::<Vec<()>>().await;
+    Ok(())
+}
+
+async fn download_posts(client: reqwest::Client, path: &PathBuf, posts: Vec<Post>) -> Result<(), Box<dyn stdError>> {
+    futures::stream::iter(posts.iter().map(|post| {
+        let cc = client.clone();
+        let p = path.clone();
+        async move {
+            match post.file.get_image_data(cc).await {
+                Ok(b) => {
+                    fs::write(format!("{}/{}.png", p.display(), post.id), b.as_ref())
+                    .unwrap();
+                },
+                Err(_) => panic!()
+            }
+        }
+    })).buffer_unordered(20).collect::<Vec<()>>().await;
+    Ok(())
+}
+
+async fn download_pools(client: reqwest::Client, path: &PathBuf, pools: Vec<&IdType>, usename: bool) -> Result<(), Box<dyn stdError>> {
+    futures::stream::iter(pools.iter().map(|id| {
+        let cc = client.clone();
+        let p = path.clone();
+        async move {
+            match Pool::new_by_id(cc.clone(), **id).await {
+                Ok(pool) => {
+                    match pool.get_all_posts(cc.clone()).await {
+                        Ok(posts) => {
+                            download_posts(cc, &p, posts).await.unwrap()
+                        }
+                        Err(_) => panic!()
+                    }
+                }
+                Err(_) => log::error!("error: pool of id {} not found", id)
+            }
+        }
+    })).buffer_unordered(5).collect::<Vec<()>>().await;
     Ok(())
 }
 
@@ -105,6 +139,9 @@ fn main() -> Result<(), Box<dyn stdError>> {
     // Starts the logger
     // TODO: change the logger implementation to env_logger
     simple_logger::SimpleLogger::new().init().unwrap();
+    
+    // Builds tokio runtime (multi-threaded)
+    let runtime = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
 
     let matches = cli().get_matches();
 
@@ -126,15 +163,15 @@ fn main() -> Result<(), Box<dyn stdError>> {
                 .flatten()
                 .collect::<Vec<_>>();
 
-            info!("These ids: {:?} to here: {:?}", ids, path);
-            let _ = download_posts(client, path, ids);
+            log::info!("These ids: {:?} to here: {:?}", ids, path);
+            let _ = runtime.block_on(download_posts_id(client, path, ids));
         }
         Some(("pools", sm)) => {
             let path = sm
                 .get_one::<PathBuf>("output")
                 .expect("defaulted in clap");
             
-            let name = sm.get_flag("name");
+            let usename = sm.get_flag("name");
 
             let ids = sm
                 .get_many::<IdType>("ID")
@@ -142,7 +179,8 @@ fn main() -> Result<(), Box<dyn stdError>> {
                 .flatten()
                 .collect::<Vec<_>>();
 
-            println!("These ids: {:?} to here: {:?} and name is {}", ids, path, name);
+            log::info!("These ids: {:?} to here: {:?} and name is {}", ids, path, usename);
+            let _ = runtime.block_on(download_pools(client, path, ids, usename));
         }
         _ => unreachable!("parser should ensure only valid subcommand names are used"),
     }    
